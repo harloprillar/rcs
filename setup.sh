260533e175335c62be35ffd30ee4e84d9a172494
harloprillar <harloprillar@gmail.com>
2020-12-23 00:18:04 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 CMD=$(which yum || which apt-get)
-$CMD install -y yamllint git wget curl
+$CMD install -qy yamllint git wget curl
 
 for f in .bashrc .vimrc .screenrc .inputrc .config/yamllint/config; do 
   curl -fsLo ~/$f --create-dirs https://raw.githubusercontent.com/harloprillar/rcs/master/home/$f
