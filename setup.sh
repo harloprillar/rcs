62051ea212fda9be6f275e31eee951d4c96739d9
harloprillar <harloprillar@gmail.com>
2020-12-23 00:15:43 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ CMD=$(which yum || which apt-get)
 $CMD install -y yamllint git wget curl
 
 for f in .bashrc .vimrc .screenrc .inputrc .config/yamllint/config; do 
-	wget https://raw.githubusercontent.com/harloprillar/rcs/master/home/$f -O ~/$f 
+  curl -fsLo ~/$f --create-dirs https://raw.githubusercontent.com/harloprillar/rcs/master/home/$f
 done;
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+curl -fsLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -E -s +PlugInstall +visual +qall
 
