e68fdf14ecd9e9696c0bc1c4aceaefd739692d51
harloprillar <harloprillar@gmail.com>
2020-12-23 00:22:15 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 CMD=$(which yum || which apt-get)
-$CMD install -qy yamllint git wget curl
+$CMD install -qy yamllint git wget curl vim vim-nox
 
 for f in .bashrc .vimrc .screenrc .inputrc .config/yamllint/config; do 
   curl -fsLo ~/$f --create-dirs https://raw.githubusercontent.com/harloprillar/rcs/master/home/$f
